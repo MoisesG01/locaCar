13db2866005f980c7411b1e4bc1250e99d2f5ced
Moises Gonçalves <moises.goncalves@fatec.sp.gov.br>
2025-04-24 17:16:17 -0300

    remove A/B test

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,23 +1,12 @@
-import { useState, useEffect } from "react";
-import styles from "./Banner.module.scss";
 import { FaCarSide } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
+import styles from "./Banner.module.scss";
 
 const Banner = () => {
-  const [version, setVersion] = useState("A"); // 'A' ou 'B'
-
-  useEffect(() => {
-    const randomVersion = Math.random() < 0.5 ? "A" : "B";
-    setVersion(randomVersion);
-  }, []);
-
-  const isVersionB = version === "B";
-
-  // Função para rastrear o clique no botão
   const handleButtonClick = () => {
     gtag("event", "click", {
       event_category: "Banner",
-      event_label: isVersionB ? "Explore Agora" : "Ver Ofertas",
+      event_label: "Explore Agora", 
     });
   };
 
@@ -25,35 +14,24 @@ const Banner = () => {
     <div
       className={styles.banner}
       style={{
-        backgroundImage: isVersionB
-          ? 'url("https://i0.wp.com/loucosporcarro.com.br/wp-content/uploads/2011/05/carro-sol-e-estrada-wallpaper-16412.jpg?ssl=1")' // Imagem para versão B
-          : 'url("https://britolocadora.com/wp-content/uploads/2023/11/oroch-brito-celular3-1024x507.png")', // Imagem para versão A
+        backgroundImage: 'url("https://i0.wp.com/loucosporcarro.com.br/wp-content/uploads/2011/05/carro-sol-e-estrada-wallpaper-16412.jpg?ssl=1")', // Imagem fixa para a versão B
       }}
     >
       <div className={styles.overlay}>
         <div className={styles.content}>
           <h1>
             <FaCarSide className={styles.icon} />
-            Encontre o <span className={styles.highlight}>
-              carro perfeito
-            </span>{" "}
-            para sua jornada
+            Encontre o <span className={styles.highlight}>carro perfeito</span> para sua jornada
           </h1>
-          {/* Alteração de subtítulo */}
           <p>
-            {isVersionB
-              ? "Descubra modelos incríveis com condições especiais para você sair dirigindo hoje."
-              : "Aproveite ofertas imperdíveis com nossa nova frota de carros."}
+            Descubra modelos incríveis com condições especiais para você sair dirigindo hoje.
           </p>
 
-          {/* CTA - Variável de Texto e Estilo */}
           <button
-            className={`${styles.cta} ${
-              isVersionB ? styles.ctaB : styles.ctaA
-            }`}
-            onClick={handleButtonClick} // Rastreando o clique
+            className={`${styles.cta} ${styles.ctaB}`} 
+            onClick={handleButtonClick} 
           >
-            {isVersionB ? "Explore Agora" : "Ver Ofertas"}{" "}
+            Explore Agora{" "}
             <FaArrowRight className={styles.arrow} />
           </button>
         </div>
@@ -63,6 +41,7 @@ const Banner = () => {
 };
 
 export default Banner;
+
 function gtag(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _arg0: string,
